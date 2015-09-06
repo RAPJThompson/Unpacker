ffd8847635426479010be465f308273432d454bf
eius <allip01@hotmail.com>
2015-09-06 13:37:18 -0700

    Adding Python version and updating README

---
+++ PythonUnpacker.py
@@ -0,0 +1,41 @@
+'''
+Created on September 6, 2015
+
+@author: RThompson
+'''
+import sys
+import os
+
+#REQUIRES: the highest directory to be dealt with and moved to, and the current directory to be worked on
+#PURPOSE: This function will recursively move through all of the files and folders in the current directory (Depth First) and 
+            #move all the files to the top directory and delete all the empty folders left behind
+def moveUp(currentDir):
+    myDirList = []
+    for subdir, dirs, files in os.walk(currentDir):                                 #walk through the whole folder and file tree starting from the given root node
+        if subdir not in myDirList:                                                 #create a list of all the directories visited
+                myDirList.append(subdir)
+        for file in files:
+            print('Moving: ' + os.path.join(subdir, file))                          #print out each found filepath
+            os.rename(os.path.join(subdir, file), os.path.join(currentDir, file))   #move this file to the top
+            
+    myDirList.reverse()                                                             #reverse the order of the files found, we want to start cleaning at the bottom
+    for myDir in myDirList:                                                         
+        if (not os.listdir(myDir)):                                                 #check whether this folder is empty
+            print ('Finishing Up In: ' + os.path.join(myDir))
+            os.rmdir(myDir)                                                         #since the folder is empty, delete it (this function won't delete a non-empty folder)
+
+#program starts here
+if (len(sys.argv) > 1):                                                             
+    currentDir =  sys.argv[1]                                                       #if there are parameters to the function call, get the first one and start there
+else:
+    currentDir = os.getcwd()                                                        #else, start work in the current directory
+    
+moveUp(currentDir)
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # Unpacker
-Simple C++ program to bring all files to the top directory. Cleaning out all the empty folders left behind.
+Simple C++/Python program to bring all files to the top directory. Cleaning out all the empty folders left behind.
 
 
 To use:
 
 Command Line:
 	
-  1) Run it without parameters to have it run in the directory that it is currently in.
+  1) Run it without parameters to have it run in the directory that you are currently working in.
 
-  2) Run it with one full directory path to have it run in the directory provided.
+  2) Run it with one full directory path parameter to have it run in the directory provided.
 	
 
 
 Non-Command Line:
 	
-  1) Running as a simple exe, ie. by double-clicking, will run the program in the folder it is currently in.
+  C++) Running as a simple exe, ie. by double-clicking, will run the program in the folder it is currently in.
+  
+  Python) Use Command Line
